ee9076401e3f4baae6faabd3a0cd5dad7fe8edc4
kayak <kayak@kayak.rocks>
2024-03-30 10:58:04 +1000

    why is my build not working?

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
-export function App() {
+const  App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { render } from 'preact'
-import { App } from './App.tsx'
 import './index.css'
+import App from './App'
 
 render(<App />, document.getElementById('app')!)
